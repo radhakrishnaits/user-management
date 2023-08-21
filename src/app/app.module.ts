19bca103306542815abda4537bfb5163add48333
Prashant Popat Sutar <psutar@altimetrik.com>
2023-08-21 19:59:24 +0530

    initial code structure

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LayoutModule } from './shared/layout/layout.module';
+import { LoginComponent } from './login/login/login.component';
+import { MaterialModule } from './shared/material.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MaterialModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
