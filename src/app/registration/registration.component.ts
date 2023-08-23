eb9bfcc35d0e80247d9da65f3d8c2b5db40a3866
Shubham Ravindra Tile <stile@altimetrik.com>
2023-08-23 16:52:59 +0530

    update with responsive form

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,8 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-import {FormControl, Validators, FormsModule, ReactiveFormsModule, FormGroup, FormBuilder} from '@angular/forms';
-import {map, startWith} from 'rxjs/operators';
-import {Observable} from 'rxjs';
+import { FormControl, Validators, FormsModule, ReactiveFormsModule, FormGroup, FormBuilder } from '@angular/forms';
+import { map, startWith } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-registration',
@@ -11,22 +11,21 @@ import {Observable} from 'rxjs';
 })
 export class RegistrationComponent {
 
-  registrationForm:any = FormGroup;
-  isCardDisplay:boolean=false;
-  hidePassword:boolean = true;
-  stateResult:any;
-  countryResult:any;
-  registrationFormDetails:any;
-  stateFilteredOptions:any = Observable<string[]>;
+  registrationForm: any = FormGroup;
+  isCardDisplay: boolean = false;
+  hidePassword: boolean = true;
+  stateResult: any;
+  countryResult: any;
+  registrationFormDetails: any;
+  stateFilteredOptions: any = Observable<string[]>;
   options: any = ['One', 'Two', 'Three'];
   myControl = new FormControl('');
   matGridCol: any;
-  matGridHeight:any;
-  constructor(private fb: FormBuilder, public http: HttpClient){
+  matGridHeight: any;
+  constructor(private fb: FormBuilder, public http: HttpClient) {
     this.http.get('assets/json/state.json').subscribe((res) => {
       this.stateResult = res;
-      console.log(this.stateResult);
-  
+      //console.log(this.stateResult);
     });
 
     this.http.get('assets/json/country.json').subscribe((res) => {
@@ -34,33 +33,31 @@ export class RegistrationComponent {
     });
   }
 
-  ngOnInit (): void {
-    
-    this.matGridCol = (window.innerWidth <= 500) ? 1 : 3;
-    this.matGridHeight = (window.innerWidth <= 500) ? "2:0.5" : "3:0.5";
+  ngOnInit(): void {
 
-    this.registrationForm = this.fb.group ({
-      userTitle : new FormControl(''),
-      firstName : new FormControl('', [Validators.required]),
-      lastName : new FormControl('', [Validators.required]),
-      dob : new FormControl('', [Validators.required]),
-      phoneNumber : new FormControl('', [Validators.required, Validators.pattern(/^[6-9]\d{9}$/)]),
-      emailId : new FormControl('', [Validators.required, Validators.email]),
-      password : new FormControl('', [Validators.required]),
-      gender : new FormControl(''),
-      nationality : new FormControl(''),
-      address : new FormControl('', [Validators.required]),
-      city : new FormControl('', [Validators.required]),
-      state : new FormControl(''),
-      pin : new FormControl(''),
-      countryofbirth : new FormControl('', [Validators.required]),
-      countryCode:new FormControl(''),
+    this.matGridCol = (window.innerWidth <= 500) ? 1 : 3;
+    this.matGridHeight = (window.innerWidth <= 800) ? "2:0.5" : "3:0.5";
+
+    this.registrationForm = this.fb.group({
+      userTitle: new FormControl(''),
+      firstName: new FormControl('', [Validators.required]),
+      lastName: new FormControl('', [Validators.required]),
+      dob: new FormControl('', [Validators.required]),
+      phoneNumber: new FormControl('', [Validators.required, Validators.pattern(/^[6-9]\d{9}$/)]),
+      emailId: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required]),
+      gender: new FormControl(''),
+      nationality: new FormControl(''),
+      address: new FormControl('', [Validators.required]),
+      city: new FormControl('', [Validators.required]),
+      state: new FormControl(''),
+      pin: new FormControl(''),
+      countryofbirth: new FormControl('', [Validators.required]),
+      countryCode: new FormControl(''),
     });
 
-    
-
-    this.stateFilteredOptions = this.myControl.valueChanges.pipe(startWith(''),map(value => this._filter(value || '')),);
-    console.log( this.stateFilteredOptions);
+    // this.stateFilteredOptions = this.myControl.valueChanges.pipe(startWith(''),map(value => this._filter(value || '')),);
+    // console.log( this.stateFilteredOptions);
 
   }
 
@@ -71,38 +68,50 @@ export class RegistrationComponent {
     return this.registrationForm.get("emailId").hasError('email') ? 'Not a valid email' : '';
   }
 
-  onBookChange(event:any){
-    this.isCardDisplay = (event.value == 'addCardY') ?  true : false;
-    console.log(this.isCardDisplay); 
+  onBookChange(event: any) {
+    this.isCardDisplay = (event.value == 'addCardY') ? true : false;
+    console.log(this.isCardDisplay);
   }
 
-  onRegistrationForm(value:any){
-    if(this.registrationForm.hasError){
-      console.log(this.registrationForm);
+  onRegistrationForm(value: any) {
+    console.log(this.registrationForm.status);
+    if (this.registrationForm.status == "INVALID") {
+      //console.log(this.registrationForm);
       alert("All fields are required");
-    }else{
-      this.registrationFormDetails = this.registrationForm.value; 
-      console.log(this.registrationFormDetails);
+    } else {
+      this.registrationFormDetails = this.registrationForm.value;
+      //console.log(JSON.stringify(this.registrationFormDetails));
     }
 
   }
 
-  onResize(event:any){
+  // onEditUser(form: any) {
+  //   console.log(form);
+  //   this.http.get('assets/json/test.json').subscribe((res) => {
+
+  //     // this.registrationForm.valueChanges = res;
+  //     this.registrationForm.patchValue(res);
+  //     // this
+
+  //   });
+  // }
+
+  onResize(event: any) {
     this.matGridCol = (event.target.innerWidth <= 500) ? 1 : 3;
-    this.matGridHeight = (window.innerWidth <= 500) ? "2:0.5" : "3:0.5";
+    this.matGridHeight = (window.innerWidth <= 800) ? "2:0.5" : "3:0.5";
   }
 
-  private _filter(value: any): any {
-    const filterValue = value;
-    console.log(filterValue);
-    this.http.get('assets/json/state.json').subscribe((res) => {
-      this.stateResult = res;
-      const result = this.stateResult.map((item:any) => item['label']);
-      console.log(result);
-      return this.options.filter((option:any) => option.toLowerCase().includes(filterValue));
-    });
-  
-  }
+  // private _filter(value: any): any {
+  //   const filterValue = value;
+  //   console.log(filterValue);
+  //   this.http.get('assets/json/state.json').subscribe((res) => {
+  //     this.stateResult = res;
+  //     const result = this.stateResult.map((item:any) => item['label']);
+  //     console.log(result);
+  //     return this.options.filter((option:any) => option.toLowerCase().includes(filterValue));
+  //   });
+
+  // }
 
 
 }
