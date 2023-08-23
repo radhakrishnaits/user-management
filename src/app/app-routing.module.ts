b73a40e7cb7b762c5b95871acb3d8b3346a37ed1
Shubham Ravindra Tile <stile@altimetrik.com>
2023-08-23 14:50:22 +0530

    added the registration form with validation and created request body as per the add registration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
 
 const routes: Routes = [
   { path: '', component: LoginComponent, pathMatch: 'full' },
-  { path: 'beneficiary', loadChildren: () => import('./beneficiary/beneficiary.module').then(m => m.BebeficiaryModule), data: { title: 'Beneficiary' } }
+  { path: 'beneficiary', loadChildren: () => import('./beneficiary/beneficiary.module').then(m => m.BebeficiaryModule), data: { title: 'Beneficiary' } },
+  { path: 'registration', component: RegistrationComponent, pathMatch: 'full'}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,25 @@ import { LayoutModule } from './shared/layout/layout.module';
 import { LoginComponent } from './login/login/login.component';
 import { MaterialModule } from './shared/material.module';
 import { BebeficiaryModule } from './beneficiary/beneficiary.module';
+import { RegistrationComponent } from './registration/registration.component';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatNativeDateModule} from '@angular/material/core';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatDividerModule} from '@angular/material/divider';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import { AsyncPipe } from '@angular/common';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule,
@@ -19,7 +33,20 @@ import { BebeficiaryModule } from './beneficiary/beneficiary.module';
     BrowserAnimationsModule,
     LayoutModule,
     BebeficiaryModule,
-    MaterialModule
+    MaterialModule,
+    MatGridListModule,
+    MatSelectModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatRadioModule,
+    MatDividerModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule,
+    MatAutocompleteModule,
+    AsyncPipe
   ],
   providers: [],
   bootstrap: [AppComponent]
